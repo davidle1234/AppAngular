6447a6d27e030d20ed031e3d5b5d4e9b276e2705
davidle1973 <davidle1234@gmail.com>
2020-02-11 19:02:35 -0500

    This is test

---
+++ AngularApp.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AgularApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgularApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
